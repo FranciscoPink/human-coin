652a91ffb772cb2c6e3a72a2c67df80df1bb099d
FranciscoPink <franciscorosadomunoz@icloud.com>
2021-08-27 09:52:07 -0400

    Go chickadee

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import logo from "./logo.jpg"
 
 export default function Nav() {
   return (
     <div>
+      <img className="logo" src={logo} alt="H-Coin" />
       <nav className="nav">
         <Link className="link" to="/">Home</Link>
         <Link className="link" to="/about">About</Link>
